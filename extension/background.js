997da434c2aa35c93f1d2ba11243e932100a5c28
mhulyalkar <miguelfhulyalkar@gmail.com>
2026-03-12 13:09:24 -0400

    feat: add right-click context menu for images

--- extension/background.js
+++ extension/background.js
@@ -1,2 +1,16 @@
 // RevSearch background service worker
-console.log("RevSearch background loaded");
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "revsearch-image",
+    title: "RevSearch this image",
+    contexts: ["image"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "revsearch-image" && info.srcUrl) {
+    chrome.sidePanel.open({ tabId: tab.id });
+    chrome.storage.local.set({ pendingSearch: { imageUrl: info.srcUrl } });
+  }
+});
